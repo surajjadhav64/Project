596d8e358d70e367d2e16decd1d7df7bc986a6e3
Suraj Rajendra Jadhav <surajjadhav57163@gmail.com>
2025-05-09 19:48:00 +0530

    Update project.py

--- project.py
+++ project.py
@@ -2,3 +2,63 @@ print("Suraj Jadhav")
 
 print("Hello World")
 
+
+
+
+
+
+
+
+
+
+
+Welcome to Cloud Shell! Type "help" to get started.
+Your Cloud Platform project in this session is set to suraj-451209.
+Use `gcloud config set project [PROJECT_ID]` to change to a different project.
+surajjadhav57163@cloudshell:~ (suraj-451209)$ git clone https://github.com/surajjadhav64/Project.git
+fatal: destination path 'Project' already exists and is not an empty directory.
+surajjadhav57163@cloudshell:~ (suraj-451209)$ cd\
+> 
+surajjadhav57163@cloudshell:~ (suraj-451209)$ rm -rf Project
+surajjadhav57163@cloudshell:~ (suraj-451209)$ ls
+Online-Exam-Management-System  README-cloudshell.txt
+surajjadhav57163@cloudshell:~ (suraj-451209)$ git clone https://github.com/surajjadhav64/Project.git
+Cloning into 'Project'...
+remote: Enumerating objects: 21, done.
+remote: Counting objects: 100% (21/21), done.
+remote: Compressing objects: 100% (16/16), done.
+remote: Total 21 (delta 1), reused 0 (delta 0), pack-reused 0 (from 0)
+Receiving objects: 100% (21/21), 7.36 KiB | 1.84 MiB/s, done.
+Resolving deltas: 100% (1/1), done.
+surajjadhav57163@cloudshell:~ (suraj-451209)$ ls
+Online-Exam-Management-System  Project  README-cloudshell.txt
+surajjadhav57163@cloudshell:~ (suraj-451209)$ cd Projct
+-bash: cd: Projct: No such file or directory
+surajjadhav57163@cloudshell:~ (suraj-451209)$ cd Online-Exam-Management-System
+surajjadhav57163@cloudshell:~/Online-Exam-Management-System (suraj-451209)$ python project.py
+python: can't open file '/home/surajjadhav57163/Online-Exam-Management-System/project.py': [Errno 2] No such file or directory
+surajjadhav57163@cloudshell:~/Online-Exam-Management-System (suraj-451209)$ cd Online-Exam-Management-System
+-bash: cd: Online-Exam-Management-System: No such file or directory
+surajjadhav57163@cloudshell:~/Online-Exam-Management-System (suraj-451209)$ ls
+ css  'DATABASE FILE'   fonts   image   js   php   README.md
+surajjadhav57163@cloudshell:~/Online-Exam-Management-System (suraj-451209)$ rm -rf Project
+surajjadhav57163@cloudshell:~/Online-Exam-Management-System (suraj-451209)$ rm -rf ^C
+surajjadhav57163@cloudshell:~/Online-Exam-Management-System (suraj-451209)$ rm -rf Online-Exam-Management-System
+surajjadhav57163@cloudshell:~/Online-Exam-Management-System (suraj-451209)$ git clone https://github.com/surajjadhav64/Project.git
+Cloning into 'Project'...
+remote: Enumerating objects: 24, done.
+remote: Counting objects: 100% (24/24), done.
+remote: Compressing objects: 100% (18/18), done.
+remote: Total 24 (delta 1), reused 0 (delta 0), pack-reused 0 (from 0)
+Receiving objects: 100% (24/24), 8.29 KiB | 8.29 MiB/s, done.
+Resolving deltas: 100% (1/1), done.
+surajjadhav57163@cloudshell:~/Online-Exam-Management-System (suraj-451209)$ ls
+ css  'DATABASE FILE'   fonts   image   js   php   Project   README.md
+surajjadhav57163@cloudshell:~/Online-Exam-Management-System (suraj-451209)$ cd Project
+surajjadhav57163@cloudshell:~/Online-Exam-Management-System/Project (suraj-451209)$ ls
+project.py  README.md
+surajjadhav57163@cloudshell:~/Online-Exam-Management-System/Project (suraj-451209)$ python project.py
+Suraj Jadhav
+Hello World
+surajjadhav57163@cloudshell:~/Online-Exam-Management-System/Project (suraj-451209)$ ^C
+surajjadhav57163@cloudshell:~/Online-Exam-Management-System/Project (suraj-451209)$ 
